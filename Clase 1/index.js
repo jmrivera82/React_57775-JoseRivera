264e2f7a282603e1cc271e67d5c56c05a37f7ea3
José Rivera <jmrivera82@gmail.com>
2024-05-23 10:33:28 -0400

    clases react

---
+++ Clase 1/index.js
@@ -0,0 +1,13 @@
+import { nombre , saludar /*, superSecurePassword */ } from "./utils.js";
+
+//document.addEventListener("DOMContentLoaded",function(){})
+console.log(nombre); // Horacio
+
+//const titulo = document.getElementById("titulo");
+//const arrTitulo = document.getElementsByClassName("title");
+//const titulo = arrTitulo[0]
+const titulo = document.querySelector("#titulo"); //null
+
+console.log(titulo)
+
+saludar()
---
+++ Clase 1/utils.js
@@ -0,0 +1,16 @@
+//utils.js
+
+export let nombre = "Horacio";
+
+let superSecurePassword = "12345678";
+
+export function saludar() {
+  console.log(superSecurePassword)
+  console.log("Hola soy " + nombre);
+}
+
+let a = 1
+let b = 2
+let suma = a + b
+
+export default suma
